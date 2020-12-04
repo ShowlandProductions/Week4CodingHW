60ae22a29f63be0fa5a67c9c3342a1636c82bed4
ShowlandProductions <35757926+ShowlandProductions@users.noreply.github.com>
2020-12-03 18:38:16 -0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var emailInput = document.querySelector("#email");
-var passwordInput = document.querySelector("#password");
-var signUpButton = document.querySelector("#sign-up");
-var msgDiv = document.querySelector("#msg");
-var userEmailSpan = document.querySelector("#user-email");
-var userPasswordSpan = document.querySelector("#user-password");
